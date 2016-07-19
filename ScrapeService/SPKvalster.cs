c590369bedd20c4e229697497ff629063209d9a9
niklasstromberg <niklasstromberg1@gmail.com>
2016-07-19 16:46:09 +0200

    Paus! Jag står HELT stilla

--- ScrapeService/SPKvalster.cs
+++ ScrapeService/SPKvalster.cs
@@ -86,67 +86,72 @@ namespace ScrapeService
 
         public List<string> GetSiteMap(string url)
         {
+            // tänkt att hämta alla sitemaps ifrån ett sitemap index
+
+
             List<string> result = new List<string>();
 
-            WebClient Client = new WebClient();
-            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", "Data.txt");
-            //Client.DownloadFile(url + "sitemap.xml", path);
+            //WebClient Client = new WebClient();
+            //Path path = Environment.SpecialFolder.Desktop;
+            //string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"Data\");
+            //Client.DownloadFile(url + "/sitemap.xml", Environment.SpecialFolder.Desktop + "Data.txt");
 
-            ScrapingBrowser Browser = new ScrapingBrowser();
-            Browser.AllowAutoRedirect = true;
-            Browser.AllowMetaRedirect = true;
+
+            //ScrapingBrowser Browser = new ScrapingBrowser();
+            //Browser.AllowAutoRedirect = true;
+            //Browser.AllowMetaRedirect = true;
             //Browser.TransferEncoding = "UTF-8";
             //string s = Browser.DownloadString(new Uri (url + "Search/sitemap"));
             string s = "";
-            WebRequest request = WebRequest.Create(url + "sitemap");
-            request.Timeout = 30 * 60 * 1000;
-            request.UseDefaultCredentials = true;
-            request.Proxy.Credentials = request.Credentials;
-            WebResponse response = request.GetResponse();
-            using (Stream stream = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader (stream, true))
-                {
-                    Encoding c = reader.CurrentEncoding;
-                    s = reader.ReadToEnd();
-                }
-                response.Close();
-            }
+            //WebRequest request = WebRequest.Create(url + "sitemap");
+            //request.Timeout = 30 * 60 * 1000;
+            //request.UseDefaultCredentials = true;
+            //request.Proxy.Credentials = request.Credentials;
+            //WebResponse response = request.GetResponse();
+            //using (Stream stream = response.GetResponseStream())
+            //{
+            //    using (StreamReader reader = new StreamReader (stream, true))
+            //    {
+            //        Encoding c = reader.CurrentEncoding;
+            //        s = reader.ReadToEnd();
+            //    }
+            //    response.Close();
+            //}
 
-
+            // ett sätt att städa xml
             s = s.Replace(Convert.ToString((byte)0x1F), "");
 
-            //string s2 = File.ReadAllText(s);
+            // ett sätt att städa xml
             s = Regex.Replace(s, @"[\u0000-\u001F]", string.Empty);
 
-            XmlDocument xml = new XmlDocument();
+            //XmlDocument xml = new XmlDocument();
             //xml.GetElementsByTagName("loc");
             //string _byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
             //if (xml.StartsWith(_byteOrderMarkUtf8))
             //{
             //    xml = xml.Remove(0, _byteOrderMarkUtf8.Length);
             //}
-            byte[] encodedString = Encoding.UTF8.GetBytes(s);
-            string joined = string.Join(",", encodedString.Select(x => x.ToString()).ToArray());
-            using (MemoryStream ms = new MemoryStream(encodedString))
-            {
-                ms.Flush();
-                ms.Position = 0;
-                xml.Load(ms);
-            }
-            
+            //byte[] encodedString = Encoding.UTF8.GetBytes(s);
+            //string joined = string.Join(",", encodedString.Select(x => x.ToString()).ToArray());
+            //using (MemoryStream ms = new MemoryStream(encodedString))
+            //{
+            //    ms.Flush();
+            //    ms.Position = 0;
+            //    xml.Load(ms);
+            //}
+
             //    int index = s.IndexOf((char)0x1F);
             //if (index > 0)
             //    xml.LoadXml(s.Substring(index, s.Length - index));
             //else
             //    xml.LoadXml(s);
             //xml.Load(url + "Search/sitemap");
-            XmlNamespaceManager manager = new XmlNamespaceManager(xml.NameTable);
-            manager.AddNamespace("s", xml.DocumentElement.NamespaceURI); //Using xml's properties instead of hard-coded URI
-            XmlNodeList xnList = xml.SelectNodes("/s:sitemapindex/s:sitemap", manager);
-            // get page
-            WebPage PageResult = Browser.NavigateToPage(new Uri(url + "Search/sitemap"));
-            
+            //XmlNamespaceManager manager = new XmlNamespaceManager(xml.NameTable);
+            //manager.AddNamespace("s", xml.DocumentElement.NamespaceURI); //Using xml's properties instead of hard-coded URI
+            //XmlNodeList xnList = xml.SelectNodes("/s:sitemapindex/s:sitemap", manager);
+            //// get page
+            //WebPage PageResult = Browser.NavigateToPage(new Uri(url + "Search/sitemap"));
+
             return result;
         }
 
--- ScrapeService/ScrapeClass.cs
+++ ScrapeService/ScrapeClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScrapySharp.Network;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Net;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Xml;
 using System.Xml.Linq;
@@ -24,18 +26,19 @@ namespace ScrapeService
 
         public ScrapeClass()
         {
-            //urls = Loop(url);
-            //XmlTextReader reader = new XmlTextReader(url);
+
+
             NumberOfScrapes = urls.Count();
-            //Data = reader.ReadOuterXml();                     // turn xml-document to readable data from passed url
+
         }
 
         // Overload for more ScrapingPatterns
         public void Scrape(SPKvalster pattern, string url)
         {
             pattern.GetSiteMap(url);
+            urls = Loop(url);
             HousingObject ho = pattern.Scrape("http://kvalster.se/Halmstad/Uthyres/L%C3%A4genheter/Snostorpsvagen_66_1775554");
-            ho.HousingId = 5;
+            ho.HousingId = 5.ToString();
             ObjectsToSave.Add(ho);
             //foreach (string url in urls)
             //{
@@ -54,69 +57,17 @@ namespace ScrapeService
         public List<string> Loop(string map)
         {
             // xml-taggen som ska hämtas är <doc>värde</doc>
-            XDocument doc = XDocument.Load(map);
-            SiteMapPath smp = new SiteMapPath();
-            //smp.SiteMapProvider;
-
-            
-
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(map);
-            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            //StreamReader sr = new StreamReader(response.GetResponseStream());
-            //XmlDocument doc = new XmlDocument();
-            ////doc.LoadXml(sr.ReadToEnd());
-            //string html = sr.ReadToEnd();
-            ////XElement sitemap = XElement.Load(sr.ReadToEnd());
-            //sr.Close();
-            //html = html.Replace(Convert.ToString((byte)0x1F), string.Empty);
-            //XmlReader reader = new XmlTextReader(html);
-            //doc = reader.ReadOuterXml(); ;
-
-            //// Download sitemap.
-            XElement sitemap = XElement.Load("http://www.gstatic.com/culturalinstitute/sitemaps/www_google_com_culturalinstitute/sitemap-index.xml");
-
-            //// ... XNames.
-            XName url = XName.Get("url", "http://www.sitemaps.org/schemas/sitemap/0.9");
-            XName loc = XName.Get("loc", "http://www.sitemaps.org/schemas/sitemap/0.9");
-
-            foreach (var urlElement in sitemap.Elements(url))
-            {
-                var locElement = urlElement.Element(loc);
-                Console.WriteLine(locElement.Value);
-            }
-
-
-            //private IEnumerable<string> GetUrls(string url)
-            //        {
-            //List<string> urls = new List<string>();
-            //XElement x = XElement.Load(new StringReader(map));
-            //XmlReader xmlReader = new XmlTextReader("http://www.gstatic.com/culturalinstitute/sitemaps/www_google_com_culturalinstitute/sitemap-index.xml");
-            //xmlReader.Read();
-            //XPathDocument document = new XPathDocument(xmlReader);
-            //XPathNavigator navigator = document.CreateNavigator();
-
-            //XmlNamespaceManager resolver = new XmlNamespaceManager(xmlReader.NameTable);
-            //resolver.AddNamespace("sitemap", "http://www.google.com/schemas/sitemap/0.9");
-
-            //XPathNodeIterator iterator = navigator.Select("/sitemap:urlset/sitemap:url/sitemap:loc", resolver);
-
-            //while (iterator.MoveNext())
-            //{
-            //    if (iterator.Current == null)
-            //        continue;
-
-            //    urls.Add(iterator.Current.Value);
-            //}
+            // tänkt att fylla listan urls med alla URL:s ifrån en sitemap
 
             return urls;
         }
-        //}
+
 
         public void SaveData(List<HousingObject> hos)
         {
             SeachService sp = new SeachService();
-            sp.DeleteIndex();
-            sp.BuildIndex();
+            //sp.DeleteIndex();
+            //sp.BuildIndex();
             sp.ListUpload(hos);
         }
     }
