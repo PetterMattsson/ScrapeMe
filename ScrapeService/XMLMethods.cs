4495c54910b4fa42485bb89e231571da41e5ad58
niklasstromberg <niklasstromberg1@gmail.com>
2016-07-28 12:14:16 +0200

    try-catch in XMLMethods

--- ScrapeService/XMLMethods.cs
+++ ScrapeService/XMLMethods.cs
@@ -12,23 +12,36 @@ namespace ScrapeService
 {
     static class XMLMethods
     {
-        public static void WriteToData(string str)
+        public static async Task<bool> WriteToData(string str)
         {
             WebClient client = new WebClient();
             string root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
             string file = string.Concat(root + @"\Data\data.txt");
 
             // Decompress sitemap and write to file     ---> TODO: keep in memorystream instead of file
-            using (Stream stream = client.OpenRead(str))
-            using (Stream tmpFile = File.Create(file))
-            using (Stream compStream = new GZipStream(stream, CompressionMode.Decompress))
+            try
             {
-                byte[] buffer = new byte[1024];
-                int nRead;
-                while ((nRead = compStream.Read(buffer, 0, buffer.Length)) > 0)
+                using (Stream stream = client.OpenRead(str))
+                using (Stream tmpFile = File.Create(file))
+                using (Stream compStream = new GZipStream(stream, CompressionMode.Decompress))
                 {
-                    tmpFile.Write(buffer, 0, nRead);
+                    byte[] buffer = new byte[1024];
+                    int nRead;
+                    if (buffer.Length < 1 || buffer == null)
+                    {
+                        return false;
+                    }
+                    while ((nRead = compStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        tmpFile.Write(buffer, 0, nRead);
+                    }
                 }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
             }
         }
 
