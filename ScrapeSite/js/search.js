f073851f70ce3ad17628ab6cc8838b56f4f44602
Petter Mattsson <petter_mattsson@hotmail.com>
2016-07-28 12:12:05 +0200

    Uppdaterad med svenska, ny formatering på bostads objekt. Cooki disclaimer tillagd. Script flyttade till search.js. Lagt till Cookie disclaimer sida.

--- ScrapeSite/js/search.js
+++ ScrapeSite/js/search.js
@@ -1,30 +1,20 @@
-/*Binding*/
+var app = angular.module('searchApp', []);
 
-$( "#searchButton" ).click(function() {
-  searchQuery($("#qV").val());
+app.controller('searchCtrl', function ($scope, $http) {
+    $scope.goSearch = function () {
+        var myUrl = "https://scrapeme.search.windows.net/indexes/housing/docs?search=" + $scope.query + "*&api-version=2015-02-28";
+        var req = {
+            method: 'GET',
+            url: myUrl,
+            headers: {
+                'Content-Type': 'application/JSON',
+                'api-key': '702D2B776E3D459E128ACCCAECD01BD9'
+            }
+        };
+
+        $http(req)
+           .then(function (response) {
+               $scope.records = response.data.value;
+           });
+    };
 });
-
-/*Query - https://scrapeme.search.windows.net/indexes/housing/docs?search=Nybro&api-version=2015-02-28*/
-function searchQuery (query){
-	var app = angular.module('searchApp', ['ngRoute']);
-	
-	app.controller('myCtrl', function($scope, $http) {
-    $http({
-        method : "GET",
-        url : "https://scrapeme.search.windows.net/indexes/housing/docs?search=Nybro&api-version=2015-02-28"
-    }).then(function mySucces(response) {
-        $scope.myResult = response.data;
-    }, function myError(response) {
-        $scope.myResult = response.statusText;
-    });
-});
-}
-/*Result generation*/
-
-
-/*
-		$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-        if ( i === 3 ) {
-          return false;
-        }
-		*/
